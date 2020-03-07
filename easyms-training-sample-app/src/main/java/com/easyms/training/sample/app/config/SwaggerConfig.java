93786260fe2a214094e9f6cb8ae10c4a630a8be4
KMIHA Ahmed <kmiha.ahmed@gmail.com>
2020-03-07 10:17:43 +0100

    kmihaahmed: add Swagger

---
+++ easyms-training-sample-app/src/main/java/com/easyms/training/sample/app/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.easyms.training.sample.app.config;
+
+
+import com.google.common.base.Predicates;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author abessa
+ */
+@Configuration
+@EnableSwagger2
+@AllArgsConstructor
+@ConditionalOnProperty(value="easyms.secured", havingValue = "false", matchIfMissing = true)
+public class SwaggerConfig {
+
+    private final SwaggerProperties properties;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select().paths(s -> Predicates.containsPattern(properties.getPaths()).apply(s))
+                .build().directModelSubstitute(ZonedDateTime.class, String.class);
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title(properties.getApiInfoTitle())
+                .description(properties.getApiInfoDescription())
+                .version(properties.getApiInfoVersion())
+                .license(properties.getApiInfoLicense())
+                .licenseUrl(properties.getApiInfoLicenseUrl())
+                .contact(new Contact(properties.getApiInfoContactName(), properties.getApiInfoContactURL(), properties.getApiInfoContactEmail()))
+                .build();
+    }
+}
